f12ef98f08ad0eb69540a6e6eacdee8e906243a5
geofis <joseramon@geografiafisica.org>
2020-06-14 12:24:43 -0400

    Added raw distance (no prediction with lm) in the output profile

--- R/profiles.R
+++ R/profiles.R
@@ -1,4 +1,4 @@
-profiles <- function(transects = transprof, height = dsm, pointsPerPixel= 2, movingAvgK = 7) {
+profiles <- function(transects = transprof, height = dsm, pointsPerPixel= 1, movingAvgK = 3) {
   library(rgeos)
   library(raster)
   library(scales)
@@ -20,33 +20,67 @@ profiles <- function(transects = transprof, height = dsm, pointsPerPixel= 2, mov
   }) %>% setNames(index)
   dfdmn <- plyr::ldply(ldmn, .id = 'transect') %>%
     na.omit %>% 
+    mutate_if(is.factor, as.character) %>% 
     group_by(transect) %>% 
-    mutate(hma=rollmean(x = h, k =  movingAvgK, na.pad=TRUE)) %>% 
-    ungroup
+    mutate(hma = rollmean(x = h, k =  movingAvgK, na.pad=TRUE)) %>% 
+    mutate(distlm = predict(lm(dist ~ hma, na.action = na.exclude)) + residuals(lm(dist ~ hma, na.action = na.exclude))) %>%
+    do(add_row(
+      .,
+      transect = unique(.$transect),
+      hma=0,
+      distlm = predict(lm(dist ~ hma, .), data.frame(hma=0)))) %>%
+    ungroup %>% 
+    mutate(transect = as.factor(transect))
   dfdmnls <- dfdmn %>% 
     group_by(transect) %>% 
+    dplyr::select(transect, distlm, hma) %>% 
     mutate(
-      dist = rescale(dist, to = c(0, 1)),
-      h = rescale(h, to = c(0, 1)),
+      distlm = rescale(distlm, to = c(0, 1)),
       hma = rescale(hma, to = c(0, 1))
       ) %>% 
     ungroup
+  dfdmnlsrawd <- dfdmn %>% 
+    group_by(transect) %>% 
+    dplyr::select(transect, dist, hma) %>% 
+    mutate(
+      dist = rescale(dist, to = c(0, 1)),
+      hma = rescale(hma, to = c(0, 1))
+    ) %>% 
+    ungroup
   dfci <- dfdmnls %>%
     group_by(transect) %>%
-    summarise(ci = (0.5 - AUC(dist, h)) / 0.5)
+    dplyr::select(transect, distlm, hma) %>% 
+    na.omit %>% 
+    summarise(ci = (0.5 - AUC(distlm, hma)) / 0.5)
+  dfcirawd <- dfdmnlsrawd %>%
+    group_by(transect) %>%
+    dplyr::select(transect, dist, hma) %>% 
+    na.omit %>% 
+    summarise(ci = (0.5 - AUC(dist, hma)) / 0.5)
   dfdmn$dist <- set_units(dfdmn$dist, 'meters')
+  dfdmn$distlm <- set_units(dfdmn$distlm, 'meters')
   dfdmn$h <- set_units(dfdmn$h, 'meters')
   dfdmn$hma <- set_units(dfdmn$hma, 'meters')
   dfsl <- dfdmn %>%
     group_by(transect) %>%
+    dplyr::select(transect, dist, distlm, hma) %>% 
     summarise(
-      dh = last(na.omit(h))-first(na.omit(h)),
-      ddist = last(na.omit(dist))-first(na.omit(dist)),
-      slope = dh/ddist, 
+      dh = max(hma, na.rm = T) - min(hma, na.rm = T),
+      ddistlm = max(distlm, na.rm = T) - min(distlm, na.rm = T),
+      slope = set_units(coef(lm(hma~distlm))[[2]], '1'),
       slopeRad = atan(slope),
       slopeDeg = set_units(slopeRad, 'degrees'),
-      xInPlot = min(dist, na.rm = T),
-      yInPlot = min(hma, na.rm = T)
+      distrawd = max(dist, na.rm = T) - min(dist, na.rm = T),
+      sloperawd = set_units(coef(lm(hma~dist))[[2]], '1'),
+      sloperawdRad = atan(sloperawd),
+      sloperawdDeg = set_units(sloperawdRad, 'degrees')
     )
-  return(list(dimension = dfdmn, dimensionless = dfdmnls, concavityindex = dfci, slope = dfsl))
+  return(
+    list(
+      dimension = dfdmn, 
+      dimensionless = dfdmnls, 
+      dimensionlessrawdistance = dfdmnlsrawd, 
+      concavityindex = dfci, 
+      concavityindexrawdistance = dfcirawd, 
+      slope = dfsl))
 }
--- R/threshold-raise-dsm.R
+++ R/threshold-raise-dsm.R
@@ -1,7 +1,4 @@
 thresholdRaise <- function(rasterDsm = NULL, threshold = NULL, outRasterPath = NULL, ...) {
-  ifelse(n>=3, 'n should be an odd number 3 or greater')
-  if (n < 3) stop('n should be an odd number 3 or greater')
-  n <- ifelse(n%%2 == 0, n+1, n)
   rasterDsm[rasterDsm < threshold] <- NA
   raisedRaster <- rasterDsm + abs(min(rasterDsm[], na.rm = T))
   if(!is.null(outRasterPath)) writeRaster(raisedRaster, outRasterPath, ...)
